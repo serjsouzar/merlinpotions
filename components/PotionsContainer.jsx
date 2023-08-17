c77caff2aeab347fddbda67b6d13509e270ce7f8
Sergio Souza <serjsouza93@gmail.com>
2023-08-16 22:10:50 -0300

    potion container title

--- components/PotionsContainer.jsx
+++ components/PotionsContainer.jsx
@@ -4,6 +4,9 @@ const PotionsContainer = ({ potions, onRequestOpen }) => {
 
   return (
     <section className="section_container">
+      <div className="potion_section_title">
+      <h1>Potions</h1>
+      </div>
       <ul className="potions_section">
         {potions?.map((potion) => (
           <li key={potion.id}>
